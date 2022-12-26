c236b728fd8b80c88d07a0a4d69f8db742cb51a2
ankit-dutta <ankitdutta965@gmail.com>
2022-12-27 01:54:17 +0530

    adding verify email

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,7 +1,45 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import AuthContext from '../store/auth-context';
 import './Welcome.css'
 
+
 const Welcome = () =>{
+
+    const authctx = useContext(AuthContext);
+
+   const verifyHandler = () =>{
+    fetch(`https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=${process.env.REACT_APP_FIREBASE_KEY}`,{
+        method:"POST",
+        body:JSON.stringify({
+            requestType:"VERIFY_EMAIL",
+            idToken:authctx.token,
+        }),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    }).then(res => {
+        if(res.ok){
+            return res.json
+        }else{
+            return res.json().then((data)=>{
+                let errorMessage = "Not Getting Fetched Data";
+                if(data && data.error && data.errorMessage){
+                    errorMessage = data.error.message
+                }
+                throw new Error(errorMessage);
+            })
+        }
+    }).then((data)=>{
+        console.log("Your Email is Verified");
+        console.log(data.email  ,"welcome email");
+    })
+    .catch((err)=>{
+        alert(err.message)
+    })
+
+    }
+
     return(
         <>
         <div>
@@ -16,6 +54,12 @@ const Welcome = () =>{
                 </div>
             </div>
             <hr />
+
+
+            <section>
+                <p>Verify your email</p>
+                <button onClick={verifyHandler} >Click here</button>
+            </section>
 
         </div>
         </>
